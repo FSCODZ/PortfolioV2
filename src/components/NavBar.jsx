3c8e82ea1ba79c73b015033b6b7270ee16efcc5c
Filip Stojanovic <filip_169@outlook.com>
2025-05-06 14:01:42 +0200

    some changes done to herosection

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          Adrian JSM
+          FSCODZ
         </a>
 
         <nav className="desktop">
@@ -44,12 +44,12 @@ const NavBar = () => {
 
         <a href="#contact" className="contact-btn group">
           <div className="inner">
-            <span>Contact me</span>
+            <span>Contact</span>
           </div>
         </a>
       </div>
     </header>
   );
-}
+};
 
 export default NavBar;
